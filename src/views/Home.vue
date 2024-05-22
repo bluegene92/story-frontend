55f57a8e92b7426386f003f50dc95b09b443b877
Dat Tran <dattran@Dats-MacBook-Pro.local>
2024-05-22 17:40:04 -0500

    Add icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -34,7 +34,22 @@
 }
 
 .section > div {
+  width: 90%;
   max-width: 600px;
+}
+
+.item {
+  display: flex;
+  gap: 32px;
+  padding: 16px 32px;
+  width: 100%;
+  border-radius: 5px;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+}
+
+.item:hover {
+  box-shadow: rgba(149, 157, 165, 0.8) 0px 8px 24px;
 }
 
 .how-to {
@@ -69,26 +84,65 @@
 
         <div>
           <ul>
-            <li class="text-green-700">
-              <h2>Personalized Tales</h2>
-              <p>Tailored to your child's name, interests, and more.</p>
+            <li class="item drop-shadow-1">
+              <div>
+                <img
+                  src="./../assets/icons/hand.png"
+                  alt="hand settings"
+                  width="60"
+                />
+              </div>
+              <div>
+                <h2>Personalized Tales</h2>
+                <p>Tailored to your child's name, interests, and more.</p>
+              </div>
             </li>
             <br />
-            <li>
-              <h2>Educational Content</h2>
-              <p>Stories designed to educate and entertain.</p>
+            <li class="item drop-shadow-1">
+              <div>
+                <img
+                  src="./../assets/icons/education.png"
+                  alt="hand settings"
+                  width="60"
+                />
+              </div>
+
+              <div>
+                <h2>Educational Content</h2>
+                <p>Stories designed to educate and entertain.</p>
+              </div>
             </li>
             <br />
-            <li>
-              <h2>Endless Adventures</h2>
-              <p>
-                A new story every time, sparking creativity and imagination.
-              </p>
+            <li class="item drop-shadow-1">
+              <div>
+                <img
+                  src="./../assets/icons/adventure-game.png"
+                  alt="hand settings"
+                  width="60"
+                />
+              </div>
+              <div>
+                <h2>Endless Adventures</h2>
+                <p>
+                  A new story every time, sparking creativity and imagination.
+                </p>
+              </div>
             </li>
             <br />
-            <li>
-              <h2>Easy to Use</h2>
-              <p>Quickly generate stories with our user-friendly interface.</p>
+            <li class="item drop-shadow-1">
+              <div>
+                <img
+                  src="./../assets/icons/convenient.png"
+                  alt="hand settings"
+                  width="60"
+                />
+              </div>
+              <div>
+                <h2>Easy to Use</h2>
+                <p>
+                  Quickly generate stories with our user-friendly interface.
+                </p>
+              </div>
             </li>
           </ul>
         </div>
@@ -105,20 +159,47 @@
 
         <div>
           <ul>
-            <li>
-              <h2>Step 1</h2>
-              <p>Enter your child's name and role</p>
+            <li class="item drop-shadow-1">
+              <div>
+                <img
+                  src="./../assets/icons/content.png"
+                  alt="hand settings"
+                  width="60"
+                />
+              </div>
+              <div>
+                <h2>Step 1</h2>
+                <p>Enter your child's name and role</p>
+              </div>
             </li>
             <br />
-            <li>
-              <h2>Step 2</h2>
-              <p>Select the genre, countries, theme, and languages</p>
+            <li class="item drop-shadow-1">
+              <div>
+                <img
+                  src="./../assets/icons/tap.png"
+                  alt="hand settings"
+                  width="60"
+                />
+              </div>
+              <div>
+                <h2>Step 2</h2>
+                <p>Select the genre, countries, theme, and languages</p>
+              </div>
             </li>
             <br />
 
-            <li>
-              <h2>Step 3</h2>
-              <p>Generate and enjoy your unique story!</p>
+            <li class="item drop-shadow-1">
+              <div>
+                <img
+                  src="./../assets/icons/magic-wand.png"
+                  alt="hand settings"
+                  width="60"
+                />
+              </div>
+              <div>
+                <h2>Step 3</h2>
+                <p>Generate and enjoy your unique story!</p>
+              </div>
             </li>
           </ul>
         </div>
